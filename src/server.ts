7fe1f12f34b457b42be8aa5a9509d5d971b79b02
Ryan Long <rlong@redhat.com>
2023-06-25 14:17:32 -0400

    add riot verification

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import Koa from "koa";
 import bodyParser from "koa-bodyparser";
+import serve from "koa-static";
 import cors from "koa2-cors";
 import logger from "koa-logger";
 import healthcheckRoutes from "./routes/health";
@@ -7,6 +8,7 @@ import mapsRoutes from "./routes/val/maps";
 import contentRoutes from "./routes/val/content";
 import statusRoutes from "./routes/val/status";
 import coinFlipRoutes from "./routes/coinflip";
+import rootRoutes from "./routes/root";
 
 import { config } from "./config";
 
@@ -22,11 +24,13 @@ app.use(
 );
 app.use(logger());
 
+app.use(rootRoutes.routes());
 app.use(healthcheckRoutes.routes());
 app.use(contentRoutes.routes());
 app.use(statusRoutes.routes());
 app.use(mapsRoutes.routes());
 app.use(coinFlipRoutes.routes());
+app.use(serve('./public'));
 
 const server = app
   .listen(PORT, async () => {
